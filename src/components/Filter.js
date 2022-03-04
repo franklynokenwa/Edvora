805e6cb2b348c178fbe671ec51086ec8628fc986
Franklyn <franklynwisdom@gmail.com>
2022-03-04 10:50:12 +0100

    displayed all states and cities in the filter drop down

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React,{useContext} from 'react';
 import StyledFilter from '../styles/Filter.styled';
 import ApiDataContext from './DataContext'
 
 
 const Filter = (props) => {
   const {filterDisplay} = props;
+  const rideData = useContext(ApiDataContext)
+  console.log(rideData);
+
+
   return (
     <StyledFilter style={{visibility: filterDisplay}}>
         <p>Filters</p>
         <hr/>
         <form>
             <select>
-                <option value="lagos">Lagos</option>
-                <option value="lagos">Lagos</option>
-                <option value="lagos">Lagos</option>
-                <option value="lagos">Lagos</option>
+              {rideData.map((items) => {
+                const {state} = items
+                return(
+                  <option value={state}>{state}</option>
+                )
+              })}
             </select>
             <select>
-                <option value="lagos">Lagos</option>
-                <option value="lagos">Lagos</option>
-                <option value="lagos">Lagos</option>
-                <option value="lagos">Lagos</option>
+              {rideData.map((items) => {
+                const {city} = items
+                return(
+                  <option value={city}>{city}</option>
+                )
+              })}
             </select>
         </form>
     </StyledFilter>
